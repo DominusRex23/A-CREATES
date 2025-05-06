d3a503561e389c2e919d56e214f7c14b154efcab
unknown <rnono2021@plm.edu.ph>
2025-05-07 03:27:07 +0800

    eslint.mjs updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // 👇 Add this block to disable the rule
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
+  
